89bf3306927318a4007e9ec1849d79894c082f3a
JulianWangHZ <julian.wang.qae@gmail.com>
2025-11-05 18:41:43 +0800

    feat: Add URL configuration settings for domain and protocol in url.py

---
+++ url.py
@@ -0,0 +1,25 @@
+from config.config import get_domain
+
+
+########## URL Path Settings ##########
+DOMAIN = get_domain()
+PROTOCOL = "https://"
+###############################
+
+
+
+########## Path Settings ##########
+
+
+
+###############################
+
+
+
+
+########## URL Settings ##########
+
+
+
+
+###############################
