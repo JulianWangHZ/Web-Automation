294b65892f318c153abfe02ca5940fc7b9c69d76
JulianWangHZ <julian.wang.qae@gmail.com>
2025-11-10 17:34:36 +0800

    fix: Correct URL construction in url.py and ensure language parameter is consistently applied in config.py for staging and dev environments

--- config/config.py
+++ config/config.py
@@ -43,11 +43,11 @@ class Config:
         
         # URL configuration - based on environment setting different BASE_PATH
         if self.ENV == 'staging':
-            default_path = '/order/-N86uOXnWsyA-7n8EKma:inline-staging-2a466/-NEdHYAxrToGxfj4BxSw'
+            default_path = '/order/-N86uOXnWsyA-7n8EKma:inline-staging-2a466/-NEdHYAxrToGxfj4BxSw?language=en'
         elif self.ENV == 'dev':
-            default_path = '/order/-N86uOXnWsyA-7n8EKma:inline-staging-2a466/-NEdHYAxrToGxfj4BxSw'
+            default_path = '/order/-N86uOXnWsyA-7n8EKma:inline-staging-2a466/-NEdHYAxrToGxfj4BxSw?language=en'
         else:
-            default_path = '/order/-N86uOXnWsyA-7n8EKma:inline-staging-2a466/-NEdHYAxrToGxfj4BxSw'
+            default_path = '/order/-N86uOXnWsyA-7n8EKma:inline-staging-2a466/-NEdHYAxrToGxfj4BxSw?language=en'
         self.BASE_PATH: str = os.getenv('BASE_PATH', default_path)
         
         # device configuration
--- url.py
+++ url.py
@@ -9,12 +9,11 @@ BASE_URL = config.BASE_URL
 
 
 ########## Path Settings ##########
-ORDER_PAGE_QUERY = "?language=en"
 ADDRESS_AND_DATE_PICKER_FRAGMENT = "#address-and-date-picker"
 ###############################
 
 
 
 ########## URL Settings ##########
-ADDRESS_AND_DATE_PICKER_URL = f"{BASE_URL}{ORDER_PAGE_QUERY}{ORDER_PAGE_QUERY}{ADDRESS_AND_DATE_PICKER_FRAGMENT}"
+ADDRESS_AND_DATE_PICKER_URL = f"{BASE_URL}{ADDRESS_AND_DATE_PICKER_FRAGMENT}"
 ###############################
